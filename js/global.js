7a106bfc85c0c00c71f42fde8dd84e717ac0d1a7
Sandor Weisz <sandor@gmail.com>
2014-02-06 18:01:37 -0600

    Added 4th task, added pseudo code to JS for task 1.

---
+++ js/global.js
@@ -0,0 +1,59 @@
+$(function() {
+    // TASK 1:
+    // When the submit button is clicked, any empty
+    // input fields should gain a red border.
+    // -----
+
+    // Capture submit event.
+
+        // Create alert on submit
+
+    // Prevent submit from occurring
+
+    // Grab all text inputs
+
+        // Output list of inputs to console
+
+    // Loop through all inputs
+
+        // Output a "got it" to console for each one
+
+    // For each input, grab value
+
+        // Output value to console
+
+    // Test if value is empty
+
+        // If it's empty, output "empty!" to console
+
+        // If it's empty, output the field itself to console
+
+    // Create new CSS class for showing empty field
+
+        // Hard-code it onto one field to test it
+
+        // Refresh. If it works, remove the hard-coded class.
+
+    // Back to JS loop. If field is empty, give it the class you defined above
+
+    // -----
+    // TASK 2:
+    // When a user clicks off of an empty input field,
+    // it should gain a red border.
+    // -----
+
+    // -----
+    // TASK 3:
+    // When a user enters text in an empty field with the
+    // red border, the red border should disappear (when field
+    // loses focus).
+    // -----
+
+    // -----
+    // TASK 4:
+    // When submitting a successful form, post it to
+    // tsljs.herokuapp.com/YOURNAME. If it works, post
+    // alert (use classes "alert" and "alert-success")
+    // on the page, saying it worked.
+    // -----
+});
