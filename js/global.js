bc5d87b9b1e5e773293a89aad07713808ef34b74
Sandor Weisz <sandor@gmail.com>
2014-02-07 18:21:26 -0600

    Wired up sign up form according to all tasks in Part 1.

--- js/global.js
+++ js/global.js
@@ -1,59 +1,44 @@
 $(function() {
-    // TASK 1:
-    // When the submit button is clicked, any empty
-    // input fields should gain a red border.
-    // -----
+    var textInputs = $('input').not('[type=radio]');
 
-    // Capture submit event.
+    $('form').submit(function(e) {
+        var formValid = true;
+        e.preventDefault();
+        textInputs.each(function (i) {
+            var userInput = $(this).val();
+            if (userInput === '') {
+                $(this).closest('.form-group').addClass('has-error');
+                formValid = false;
+            }
+        });
 
-        // Create alert on submit
+        if (formValid) {
+            // Grab all the form data and turn it into a JSON object.
+            var formData = $(this).serializeArray();
+            $('.alert').remove();
+            $.post(
+                'http://tsljs.herokuapp.com/elvis',
+                formData,
+                function(response) {
+                    // The response from the serve looks like JSON, but is a string. We need to use this function to turn it from a string into JSON.
+                    responseAsJSON = $.parseJSON(response);
+                    if (responseAsJSON['status'] == 1) {
+                        $('h1').after('<div class="alert alert-success">Success!</div>');
+                        $('form')[0].reset();
+                    }
+                }
+            );
+        }
+    });
 
-    // Prevent submit from occurring
-
-    // Grab all text inputs
-
-        // Output list of inputs to console
-
-    // Loop through all inputs
-
-        // Output a "got it" to console for each one
-
-    // For each input, grab value
-
-        // Output value to console
-
-    // Test if value is empty
-
-        // If it's empty, output "empty!" to console
-
-        // If it's empty, output the field itself to console
-
-    // Create new CSS class for showing empty field
-
-        // Hard-code it onto one field to test it
-
-        // Refresh. If it works, remove the hard-coded class.
-
-    // Back to JS loop. If field is empty, give it the class you defined above
-
-    // -----
-    // TASK 2:
-    // When a user clicks off of an empty input field,
-    // it should gain a red border.
-    // -----
-
-    // -----
-    // TASK 3:
-    // When a user enters text in an empty field with the
-    // red border, the red border should disappear (when field
-    // loses focus).
-    // -----
-
-    // -----
-    // TASK 4:
-    // When submitting a successful form, post it to
-    // tsljs.herokuapp.com/YOURNAME. If it works, post
-    // alert (use classes "alert" and "alert-success")
-    // on the page, saying it worked.
-    // -----
+    textInputs.focusout(function(e) {
+        var userInput = $(this).val();
+        if (userInput === '') {
+            $(this).closest('.form-group').addClass('has-error');
+        } else {
+            $(this).closest('.form-group').removeClass('has-error');
+        }
+    }).keypress(function (e) {
+        $(this).closest('.form-group').removeClass('has-error');
+    });
 });
